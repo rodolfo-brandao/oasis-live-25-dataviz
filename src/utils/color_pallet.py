e529be36ac52524f5f5511598a6941f07d48851d
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-03-27 19:18:13 -0300

    chore: remove duplicated dicts

--- src/utils/color_pallet.py
+++ src/utils/color_pallet.py
@@ -6,19 +6,6 @@ Everything related to colors.
 from typing import List, Dict
 
 
-# OASIS_ALBUM_COLORS: Dict[str, str] = {
-#     "Definitely Maybe":                     "#222f5b",
-#     "Morning Glory":                        "#3d3d3d",
-#     "Be Here Now":                          "#4a7a8a",
-#     "The Masterplan":                       "#5c6e8a",
-#     "Standing on the Shoulder of Giants":   "#6b7f8c",
-#     "Heathen Chemistry":                    "#7a6a5a",
-#     "Don't Believe the Truth":              "#5a4f47",
-#     "Dig Out Your Soul":                    "#395e7f",
-#     "Other / Unknown":                      "#bcb5ae"
-# }
-
-
 OASIS_ALBUM_COLORS: Dict[str, str] = {
     "Definitely Maybe":                     "#222f5b",
     "Morning Glory":                        "#3d3d3d",
@@ -29,17 +16,6 @@ OASIS_ALBUM_COLORS: Dict[str, str] = {
     "Don't Believe the Truth":              "#4a6fa5",
     "Dig Out Your Soul":                    "#2c4a7c",
     "Other / Unknown":                      "#5a5a5a"
-}
-
-
-_OASIS_COLOR_PALLET: Dict[str, str] = {
-    "primary":      "#222f5b",
-    "secondary":    "#395e7f",
-    "accent":       "#271d12",
-    "neutral":      "#3d3d3d",
-    "light":        "#bcb5ae",
-    "background":   "#f7f6f3",
-    "grid":         "#d9d4ce"
 }
 
 
