e0e97335df161a929ef315455127b952ec079bb2
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-03-28 15:03:30 -0300

    feat: add more summary info

--- src/dashboard.py
+++ src/dashboard.py
@@ -26,7 +26,7 @@ st.markdown(
 with st.container(border=True):
     # ===== Section 1 (container) =====
     st.subheader(
-        body="Main Statistics"
+        body="Summary"
     )
 
     (
@@ -34,34 +34,39 @@ with st.container(border=True):
         sec1_col2,
         sec1_col3,
         sec1_col4,
-    ) = st.columns(4, gap="small")
+        sec1_col5
+    ) = st.columns(5, gap="small")
 
     with sec1_col1:
         st.metric(
             label="🗓️ First Concert Date",
-            value=pd.to_datetime(
-                df["date"], errors="coerce"
-            ).min().strftime(format="%B %d, %Y")
+            value=pd.to_datetime(df["date"], errors="coerce")
+            .min().strftime(format="%B %d, %Y")
         )
 
     with sec1_col2:
         st.metric(
             label="🗓️ Last Concert Date",
-            value=pd.to_datetime(
-                df["date"], errors="coerce"
-            ).max().strftime(format="%B %d, %Y")
+            value=pd.to_datetime(df["date"], errors="coerce")
+                .max().strftime(format="%B %d, %Y")
         )
 
     with sec1_col3:
         st.metric(
-            label="🌎 Total Countries",
-            value=len(df.groupby("country_or_constituent_country").sum())
+            label="🎸 Total Concerts",
+            value=len(df)
         )
 
     with sec1_col4:
         st.metric(
-            label="🎸 Total Concerts",
-            value=len(df)
+            label="🌎 Total Countries",
+            value=df['country_or_constituent_country'].nunique()
+        )
+
+    with sec1_col5:
+        st.metric(
+            label="🏙️ Total Cities",
+            value=df['city'].nunique()
         )
 
     # ===== Section 2 (container) =====
@@ -69,31 +74,38 @@ with st.container(border=True):
         sec2_col1,
         sec2_col2,
         sec2_col3,
-        sec2_col4
-    ) = st.columns(4, gap="small")
+        sec2_col4,
+        sec2_col5
+    ) = st.columns(5, gap="small")
 
     with sec2_col1:
         st.metric(
-            label="📍 Country with Most Concerts",
-            value=df["country_or_constituent_country"].value_counts().idxmax()
+            label="🎫 Avg. Ticket Price (USD)",
+            value=f"${df["avg_ticket_price_usd_pollstar"].unique()[0]}"
         )
 
     with sec2_col2:
         st.metric(
-            label="🎫 Avg. Ticket Price (USD)",
-            value="$" + str(df["avg_ticket_price_usd_pollstar"].unique()[0])
+            label="💰 Total Estimated Gross (USD)",
+            value=f"${df["gross_estimated_usd_per_concert"].sum():,.0f}"
         )
 
     with sec2_col3:
         st.metric(
-            label="🔻 Lowest Attendance",
+            label="🔻 Lowest Attendance per Concert",
             value=f"{df["attendance_estimated_per_concert"].min():,}"
         )
 
     with sec2_col4:
         st.metric(
-            label="🔺 Highest Attendance",
+            label="🔺 Highest Attendance per Concert",
             value=f"{df["attendance_estimated_per_concert"].max():,}"
+        )
+
+    with sec2_col5:
+        st.metric(
+            label="🧑‍🧑‍🧒‍🧒 Total Attendance",
+            value=f"{df["attendance_estimated_per_concert"].sum():,}"
         )
 
 
@@ -164,20 +176,20 @@ with sec5_col1:
 
 with sec5_col2:
     st.subheader(
-        body="Song Frequency by LP/EP"
+        body="Frequency of LPs/EPs per Song Played"
     )
 
     st.plotly_chart(
         figure_or_data=charts_factory.create_song_frequency_by_album_chart(df)
     )
 
-# ===== Section X ===== (last one)
+# ===== Section 6 =====
 with st.container(border=True):
     st.subheader(
         body="Raw Data Sample"
     )
 
-    columns_to_drop = [
+    cols_to_drop = [
         "concert_id",
         "attendance_scope_note",
         "gross_reported_usd_per_concert",
@@ -188,5 +200,5 @@ with st.container(border=True):
     ]
 
     st.dataframe(
-        data=df.drop(columns=columns_to_drop).head(n=10)
+        data=df.drop(columns=cols_to_drop).head(n=10)
     )
